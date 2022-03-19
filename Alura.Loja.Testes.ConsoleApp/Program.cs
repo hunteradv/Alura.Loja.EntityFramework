52926c71ee734f941a2e9594a0274ba2a85f92da
hunteradv <gustavopaulody@gmail.com>
2022-03-19 01:08:39 -0300

    Corrigindo problemas com as versões do entity diferente entre os projetos.

--- Alura.Loja.Testes.ConsoleApp/Program.cs
+++ Alura.Loja.Testes.ConsoleApp/Program.cs
@@ -73,18 +73,5 @@ namespace Alura.Loja.Testes.ConsoleApp
                 context.Add(p);
             }
         }
-
-        private static void GravarUsandoAdoNet()
-        {
-            Produto p = new Produto();
-            p.Nome = "Harry Potter e a Ordem da Fênix";
-            p.Categoria = "Livros";
-            p.Preco = 19.89;
-
-            using (var repo = new ProdutoDAO())
-            {
-                repo.Add(p);
-            }
-        }
     }
 }
